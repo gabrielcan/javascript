51c89d5138eb8ac8e967d544093786a1c1cdffb0
Gabriel Cano <gabrielcano.86@gmail.com>
2019-10-18 13:25:54 -0300

    funciona_OK_Switch

--- js/codigo.js
+++ js/codigo.js
@@ -27,25 +27,35 @@ var p2;
 
 function operaciones() {
        
-    
+        
     var check=document.getElementById('selection').value;
-    
+    p1= document.getElementById('valor1').value;
+    p2=document.getElementById('valor2').value;
+
 
     check=parseInt(check);
-    console.log(check);
-    
+   // console.log(check);
+
 switch (check) {
     case 1:
-            suma();
+            console.log('estoy en caso1');
+            
+            suma(p1,p2);
         break;
     case 2:
-            resta();   
+            console.log('estoy en caso2');
+            
+            resta(p1,p2);   
         break;
     case 3:
-            div();
+            console.log('estoy en caso3');
+            
+            div(p1,p2);
         break;
     case 4:
-            mult();
+            console.log('estoy en caso4');
+        
+            mult(p1,p2);
         break;
                         
     default:
@@ -56,36 +66,35 @@ switch (check) {
 
 
             function suma(p1,p2) {
-                p1= document.getElementById('valor1').value;
-                p2=document.getElementById('valor2').value;
-                    
-                var resultadoMult=parseInt(p1+p2);
-                // console.log(resultadoMult); 
+                p1=parseInt(p1);
+                p2=parseInt(p2);   
+                var resultadoSum=parseInt(p1+p2);
+                
+                //console.log(resultadoSum); 
+                alert(resultadoSum);
                                 }
 
             function resta(p1,p2) {
-                p1= document.getElementById('valor1').value;
-            p2=document.getElementById('valor2').value;
+                
             
-            var resultadoMult=parseInt(p1-p2);
-            // console.log(resultadoMult); 
-            
+            var resultadoRes=parseInt(p1-p2);
+            //console.log(resultadoRes); 
+            alert(resultadoRes);
             }
         
             function div(p1,p2) {
-                p1= document.getElementById('valor1').value;
-            p2=document.getElementById('valor2').value;
+             
             
-            var resultadoMult=p1/p2;
-            // console.log(resultadoMult); 
+            var resultadoDiv=p1/p2;
+            //console.log(resultadoDiv); 
+            alert(resultadoDiv);
                                 }
             function mult(p1,p2){
 
-            p1= document.getElementById('valor1').value;
-            p2=document.getElementById('valor2').value;
                 
             var resultadoMult=p1*p2;
-            // console.log(resultadoMult);
+            //console.log(resultadoMult);
+            alert(resultadoMult);
     
                                 }
 //fin funciones
